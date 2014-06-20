27bc518fec89b4ea579a9d73415b70ffb478482d
Philip Smith <philipsmith@Philip-Smiths-MacBook-Pro.local>
2014-06-20 17:46:56 +0100

    added test class

---
+++ Git Test/LHTest.h
@@ -0,0 +1,13 @@
+//
+//  LHTest.h
+//  Git Test
+//
+//  Created by Philip Smith on 20/06/2014.
+//  Copyright (c) 2014 Nick Smith. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LHTest : NSObject
+
+@end
---
+++ Git Test/LHTest.m
@@ -0,0 +1,13 @@
+//
+//  LHTest.m
+//  Git Test
+//
+//  Created by Philip Smith on 20/06/2014.
+//  Copyright (c) 2014 Nick Smith. All rights reserved.
+//
+
+#import "LHTest.h"
+
+@implementation LHTest
+
+@end
